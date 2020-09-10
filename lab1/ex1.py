3fa29b5f304904c5f2e258649f68999090580c65
ryy <2524380603@qq.com>
2020-09-10 21:32:05 +0800

    added a ex1.py file (山居秋暝)

---
+++ lab1/ex1.py
@@ -0,0 +1,13 @@
+b=input("============ RESTART: C")
+while(b=='c' or b=='C'):
+    a = input("山居秋暝的作者是谁？")
+    if(a=="王维"):
+        print("Bingo")
+        print("山居秋暝")
+        print(" 王维")
+        print("空山新雨后，")
+        print("天气晚来秋，")
+        print("明月松间照，")
+        print("清泉石上流。")
+    else:
+        print("对不起，打错啦！")
