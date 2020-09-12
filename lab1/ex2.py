4476542dae568049ea7c786cf587a6474deb10e7
ryy <2524380603@qq.com>
2020-09-12 10:07:07 +0800

    字符串处理

---
+++ lab1/ex2.py
@@ -0,0 +1,14 @@
+one_price=9
+two_price=20
+three_price=11
+total=one_price+\
+      two_price+\
+      three_price                 #分行计算
+print(one_price,two_price,three_price,total)    #输出
+name='Tom is a cat!'                
+print(name[0:2])
+print(name[4:6])
+print(name[-1])
+print(name)                         #字符串值读取
+a=len(name)                         #字符串长度读取
+print(a)
